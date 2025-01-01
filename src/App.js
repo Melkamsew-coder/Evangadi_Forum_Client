465dd2ae77fa12ce465a7f4e303f68a03e77edac
Melkamsew-coder <melkamsew27@gmail.com>
2025-01-01 12:27:50 -0700

    Fix useEffect dependency issue by wrapping checkUser with useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,55 @@
-import { createContext, useEffect, useState } from "react";
+// import { createContext, useEffect, useState } from "react";
+// import "./App.css";
+// import Home from "./pages/Home/Home";
+// // import Login from "./pages/Login/Login";
+// // import Register from "./pages/Register/Register";
+// import { Route, Routes, useNavigate } from "react-router-dom";
+// import axios from "./Api/axiosConfig";
+// import Landing from "./pages/Landing/Landing";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Ask from "./pages/Ask/Ask";
+// import Answer from "./pages/Answer/Answer";
+
+// export const AppState = createContext();
+
+// function App() {
+//   const [user, setUser] = useState({});
+//   const token = localStorage.getItem("token");
+
+//   const navigate = useNavigate();
+
+//   async function checkUser() {
+//     try {
+//       const { data } = await axios.get("/users/checkUser", {
+//         headers: {
+//           Authorization: "Bearer " + token,
+//         },
+//       });
+//       setUser(data);
+//     } catch (error) {
+//       console.log(error.response);
+//       navigate("/");
+//     }
+//   }
+
+//   useEffect(() => {
+//     checkUser();
+//   }, []);
+
+//   return (
+//     <AppState.Provider value={{ user, setUser }}>
+//       <Routes>
+//         <Route path="/" element={<Landing />} />
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/ask" element={<Ask />} />
+//         <Route path="/question/:questionid" element={<Answer />} />
+//       </Routes>
+//     </AppState.Provider>
+//   );
+// }
+
+// export default App;
+import { createContext, useEffect, useState, useCallback } from "react";
 import "./App.css";
 import Home from "./pages/Home/Home";
 // import Login from "./pages/Login/Login";
@@ -18,7 +69,7 @@ function App() {
 
   const navigate = useNavigate();
 
-  async function checkUser() {
+  const checkUser = useCallback(async () => {
     try {
       const { data } = await axios.get("/users/checkUser", {
         headers: {
@@ -30,11 +81,11 @@ function App() {
       console.log(error.response);
       navigate("/");
     }
-  }
+  }, [token, navigate]);
 
   useEffect(() => {
     checkUser();
-  }, []);
+  }, [checkUser]);
 
   return (
     <AppState.Provider value={{ user, setUser }}>
